1e42db34e07be6caccea0fb09afaed27958175f1
Jatin Parmar <parmarjatin276@gmail.com>
2024-01-27 12:28:50 +0530

    fix1.9

--- app/student-dashboard/page.js
+++ app/student-dashboard/page.js
@@ -5,7 +5,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import NavbarMain from '../../components/NavbarMain';
 import { useSelector } from 'react-redux';
-export const StudentDashboardPage = () => {
+const StudentDashboardPage = () => {
     const [sPresent, setSPresent] = useState(0);
     const[check,setCheck] = useState(false);
     const [error, setError] = useState(null);
