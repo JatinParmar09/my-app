5958920b54a9b7ed27e1aa4f31fa81732fe176f6
Jatin Parmar <parmarjatin276@gmail.com>
2024-01-27 11:28:14 +0530

    fix1.3

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -7,6 +7,7 @@ import NavbarMain from '../../components/NavbarMain';
 import { UseSelector, useSelector } from 'react-redux';
 import { format } from 'date-fns';
 export const DashboardPage = () => {
+  // eslint-disable-next-line react/no-unescaped-entities
   const [error, setError] = useState(null);
   const user = useSelector(state => state.user);
   const[check,setCheck] = useState(false);
--- app/dashboard/qrpage/page.js
+++ app/dashboard/qrpage/page.js
@@ -4,70 +4,71 @@ import QRCode from 'qrcode.react';
 import NavbarMain from '../../../components/NavbarMain';
 import axios from 'axios';
 
-const qrpage = () => {
-    const[check,setCheck] = useState(false);
-    useEffect( () => {
-      const cookieValue = document.cookie.split('=')[1];
-      const headers = {
-        Authorization: `Bearer ${cookieValue}`
-      }
-       axios.get("https://flipr-yi8b.onrender.com/api/test2",
-       {headers}
-       )
-        .then((response) => {
-          console.log("SUCCESS");
-          setCheck(true);})
-        .catch((error) => {
-          window.location.href = '/';    
+const QRPage = () => {
+  const [check, setCheck] = useState(false);
+  const [url, setUrl] = useState('');
+  const [socket, setSocket] = useState(null);
+  useEffect(() => {
+    const cookieValue = document.cookie.split('=')[1];
+    const headers = {
+      Authorization: `Bearer ${cookieValue}`
     }
-      ); 
-    }, []);
-    const [url, setUrl] = useState('');
-    const [socket, setSocket] = useState(null);
+    axios.get("https://flipr-yi8b.onrender.com/api/test2",
+      { headers }
+    )
+      .then((response) => {
+        console.log("SUCCESS");
+        setCheck(true);
+      })
+      .catch((error) => {
+        window.location.href = '/';
+      }
+      );
+  }, []);
 
-    useEffect(() => {
-       const ws = new WebSocket('ws://flipr-yi8b.onrender.com');
-   
-       ws.onopen = () => {
-         console.log('Connection established');
-       };
-   
-       ws.onmessage = (event) => {
-           console.log('Message from server: ', event.data);
-           setUrl(event.data);
-           
-       };
-   
-       setSocket(ws);
-   
-       return () => {
-         ws.close();
-       };
-    }, []);
-    const sendMessage = () => {
-       if (socket) {
-         socket.send('Your message');
-       }         
+  useEffect(() => {
+    const ws = new WebSocket('ws://flipr-yi8b.onrender.com');
+
+    ws.onopen = () => {
+      console.log('Connection established');
     };
 
-    return (
-        check ? (
-        <>
-            <header>
-                <NavbarMain />
-            </header>
-            <main>
-                <div className="flex flex-col items-center justify-center h-[75vh] bg-gray-100">
-                    <div className="shadow-md rounded-lg bg-white p-4">
-                        <h1 className="text-2xl font-bold mb-2">Scan to mark attendance</h1>
-                        <div className="flex justify-center items-center mb-4">
-                        {(url==='WAIT FOR ONE MINUTE' || url===''?(<p>WAIT FOR ONE MINUTE</p>): <QRCode value={url} size={200}/>) }
-                        </div>
-                    </div>
-                </div>
-            </main>
-        </>) : <></>
-    )
+    ws.onmessage = (event) => {
+      console.log('Message from server: ', event.data);
+      setUrl(event.data);
+
+    };
+
+    setSocket(ws);
+
+    return () => {
+      ws.close();
+    };
+  }, []);
+  const sendMessage = () => {
+    if (socket) {
+      socket.send('Your message');
+    }
+  };
+
+  return (
+    check ? (
+      <>
+        <header>
+          <NavbarMain />
+        </header>
+        <main>
+          <div className="flex flex-col items-center justify-center h-[75vh] bg-gray-100">
+            <div className="shadow-md rounded-lg bg-white p-4">
+              <h1 className="text-2xl font-bold mb-2">Scan to mark attendance</h1>
+              <div className="flex justify-center items-center mb-4">
+                {(url === 'WAIT FOR ONE MINUTE' || url === '' ? (<p>WAIT FOR ONE MINUTE</p>) : <QRCode value={url} size={200} />)}
+              </div>
+            </div>
+          </div>
+        </main>
+      </>) : <></>
+  )
 }
 
-export default qrpage
+export default QRPage
